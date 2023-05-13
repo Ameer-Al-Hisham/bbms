3327ad7cb71bba074462af2f591ff62671ccf3ff
Mrinal Sharma <mekmun2018@gmail.com>
2023-05-13 20:48:15 +0530

    created donor view and request form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+
+
+
+import './App.css';
 import Home from './components/Home';
 import Login from'./components/Login';
+import Requestform from './components/Requestform';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import Userview from './components/Userview';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+       <Userview/>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
+          <Route path="/form" element={<Requestform />}></Route> 
         </Routes>
       </BrowserRouter>
     </div>
