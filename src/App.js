d46484d687dc8ee2d76a15d9f9204b13d5632754
Aquila <reenacottage@gmail.com>
2023-05-17 11:17:49 +0530

    signup page routed

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Requestform from './components/Requestform';
 import Userview from './components/Userview';
+import Signup from './components/Signup';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-       <Userview/>
+        <Userview />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/form" element={<Requestform />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,46 +1,82 @@
-import React from 'react'
-import { Avatar, Button, Checkbox, FormControlLabel, Grid, Link, Paper, TextField, Typography } from '@mui/material';
-import { LockOutlined } from '@mui/icons-material';
+import React from "react";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { LockOutlined } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
-const Login=()=>{ 
-
-    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
-    const avatarStyle={backgroundColor:'#1bbd7e'}
-    const btnstyle={margin:'8px 0'}
-    return(
-        <Grid>
-            <Paper elevation={10} style={paperStyle}>
-                <Grid align='center'>
-                     <Avatar style={avatarStyle}><LockOutlined/></Avatar>
-                   <Typography variant='h4' gutterBottom color='#880e4f' sx={{ textDecoration: 'underline' }}>Sign In</Typography>
-                </Grid>
-                <TextField label='Email' placeholder='Enter email' type='email' fullWidth required/>
-                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
-                <FormControlLabel
-                    control={
-                    <Checkbox
-                        name="checkedB"
-                        color="primary"
-                    />
-                    }
-                    label="Remember me"
-                 />
-                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
-                <Typography >
-                   <Link href="#" >
-                        Forgot password ?
-                </Link>
-                </Typography>
-                <Typography > Don't have an account ?
-                <Button variant="contained" color="success">
-                <Link href="#" >
-                        Create Account
-                </Link>
-                </Button>
-                </Typography>
-            </Paper>
+const Login = () => {
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 280,
+    margin: "20px auto",
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const btnstyle = { margin: "8px 0" };
+  return (
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <LockOutlined />
+          </Avatar>
+          <Typography
+            variant="h4"
+            gutterBottom
+            color="#880e4f"
+            sx={{ textDecoration: "underline" }}
+          >
+            Sign In
+          </Typography>
         </Grid>
-    )
-}
+        <TextField
+          label="Email"
+          placeholder="Enter email"
+          type="email"
+          fullWidth
+          required
+        />
+        <TextField
+          label="Password"
+          placeholder="Enter password"
+          type="password"
+          fullWidth
+          required
+        />
+        <FormControlLabel
+          control={<Checkbox name="checkedB" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          style={btnstyle}
+          fullWidth
+        >
+          Sign in
+        </Button>
+        <Typography>
+          <Link href="#">Forgot password ?</Link>
+        </Typography>
+        <Typography>
+          {" "}
+          Don't have an account ?
+          <Button variant="contained" color="success">
+            <Link to="/signup">Create Account</Link>
+          </Button>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
+};
 
-export default Login
+export default Login;
